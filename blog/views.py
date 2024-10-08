3cb7bbc7df5fc568a19f342a86454c1d7ce93d8a
Juan Arillo <juan.arillo@apppadel.com>
2024-10-08 09:59:25 +0200

    finished chapter 2

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm,EmailPostForm
 from .models import Post
 
 # Create your views here.
@@ -31,7 +32,14 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post, 'comments': comments, 'form': form}
+    )
 
 class PostListView(ListView):
     """
@@ -71,4 +79,25 @@ def post_share(request, post_id):
         request,
         'blog/post/share.html',
         {'post': post, 'form': form, 'sent': sent}
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment,
+        }
     )
